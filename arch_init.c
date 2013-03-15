fe0f93e480b8578421f19b394941259915a568c7
Peter Lieven <pl@kamp.de>
2013-03-15 14:23:37 +0100

    fix offset by one
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- arch_init.c
+++ arch_init.c
@@ -544,7 +544,7 @@ static void reset_ram_globals(void)
 {
     last_seen_block = NULL;
     last_sent_block = NULL;
-    last_offset = 0;
+    last_offset = -1;
     last_version = ram_list.version;
     ram_bulk_stage = true;
 }
