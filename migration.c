24c0a850ba3169e1bcb43de3b2a6480157df9dcc
Peter Lieven <pl@kamp.de>
2013-07-15 10:10:30 +0200

    block-migration: efficiently encode zero blocks
    this patch adds a efficient encoding for zero blocks by
    adding a new flag indiciating a block is completly zero.
    
    additionally bdrv_write_zeros() is used at the destination
    to efficiently write these zeroes.
    
    v2->v3:
     - changed type of flags in blk_send() from int to uint64_t
     - added migration capability setting to enable sending
       of zero blocks.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- migration.c
+++ migration.c
@@ -484,6 +484,15 @@ bool migrate_rdma_pin_all(void)
     return s->enabled_capabilities[MIGRATION_CAPABILITY_X_RDMA_PIN_ALL];
 }
 
+bool migrate_zero_blocks(void)
+{
+    MigrationState *s;
+
+    s = migrate_get_current();
+
+    return s->enabled_capabilities[MIGRATION_CAPABILITY_ZERO_BLOCKS];
+}
+
 int migrate_use_xbzrle(void)
 {
     MigrationState *s;
