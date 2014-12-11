4e78b1595467b27fbac71e41b20cae287596a08e
Kevin Wolf <kwolf@redhat.com>
2014-12-11 10:55:57 +0100

    block: Expose blk_co_readv/writev()
    Allow users that are already in coroutine context to call the block
    layer with the most natural interface.
    
    Signed-off-by: Kevin Wolf <kwolf@redhat.com>

--- block/block-backend.c
+++ block/block-backend.c
@@ -493,6 +493,18 @@ BlockAIOCB *blk_aio_ioctl(BlockBackend *blk, unsigned long int req, void *buf,
     return bdrv_aio_ioctl(blk->bs, req, buf, cb, opaque);
 }
 
+int coroutine_fn blk_co_readv(BlockBackend *blk, int64_t sector_num,
+                              int nb_sectors, QEMUIOVector *qiov)
+{
+    return bdrv_co_readv(blk->bs, sector_num, nb_sectors, qiov);
+}
+
+int coroutine_fn blk_co_writev(BlockBackend *blk, int64_t sector_num,
+                               int nb_sectors, QEMUIOVector *qiov)
+{
+    return bdrv_co_writev(blk->bs, sector_num, nb_sectors, qiov);
+}
+
 int blk_co_discard(BlockBackend *blk, int64_t sector_num, int nb_sectors)
 {
     return bdrv_co_discard(blk->bs, sector_num, nb_sectors);
