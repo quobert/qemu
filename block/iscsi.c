b4b0e0db7069b1fe8fe977607bb2ab94335ce61b
Peter Lieven <pl@kamp.de>
2015-04-16 10:04:45 +0200

    block/iscsi: handle SCSI_STATUS_TASK_SET_FULL
    a target may issue a SCSI_STATUS_TASK_SET_FULL status
    if there is more than one "BUSY" command queued already.
    
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- block/iscsi.c
+++ block/iscsi.c
@@ -185,10 +185,10 @@ iscsi_co_generic_cb(struct iscsi_context *iscsi, int status,
                 iTask->do_retry = 1;
                 goto out;
             }
-            if (status == SCSI_STATUS_BUSY) {
+            if (status == SCSI_STATUS_BUSY || status == SCSI_STATUS_TASK_SET_FULL) {
                 unsigned retry_time =
                     exp_random(iscsi_retry_times[iTask->retries - 1]);
-                error_report("iSCSI Busy (retry #%u in %u ms): %s",
+                error_report("iSCSI Busy/TaskSetFull (retry #%u in %u ms): %s",
                              iTask->retries, retry_time,
                              iscsi_get_error(iscsi));
                 aio_timer_init(iTask->iscsilun->aio_context,
