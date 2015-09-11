94e1e795fa41658378911ad2ecee26370aeda297
Gerd Hoffmann <kraxel@redhat.com>
2015-09-11 14:35:44 +0200

    io: add qio_buffer tracing
    Signed-off-by: Gerd Hoffmann <kraxel@redhat.com>

--- io/buffer.c
+++ io/buffer.c
@@ -19,6 +19,7 @@
  */
 
 #include "io/buffer.h"
+#include "trace.h"
 
 #define QIO_BUFFER_MIN_INIT_SIZE     4096
 #define QIO_BUFFER_MIN_SHRINK_SIZE  65536
@@ -34,6 +35,8 @@ void qio_buffer_init(QIOBuffer *buffer, const char *name, ...)
 
 void qio_buffer_shrink(QIOBuffer *buffer)
 {
+    size_t old;
+
     /*
      * Only shrink in case the used size is *much* smaller than the
      * capacity, to avoid bumping up & down the buffers all the time.
@@ -44,17 +47,25 @@ void qio_buffer_shrink(QIOBuffer *buffer)
         return;
     }
 
+    old = buffer->capacity;
     buffer->capacity = pow2ceil(buffer->offset);
     buffer->capacity = MAX(buffer->capacity, QIO_BUFFER_MIN_SHRINK_SIZE);
     buffer->buffer = g_realloc(buffer->buffer, buffer->capacity);
+    trace_qio_buffer_resize(buffer->name ?: "unnamed",
+                            old, buffer->capacity);
 }
 
 void qio_buffer_reserve(QIOBuffer *buffer, size_t len)
 {
+    size_t old;
+
     if ((buffer->capacity - buffer->offset) < len) {
+        old = buffer->capacity;
         buffer->capacity = pow2ceil(buffer->offset + len);
         buffer->capacity = MAX(buffer->capacity, QIO_BUFFER_MIN_INIT_SIZE);
         buffer->buffer = g_realloc(buffer->buffer, buffer->capacity);
+        trace_qio_buffer_resize(buffer->name ?: "unnamed",
+                                old, buffer->capacity);
     }
 }
 
@@ -75,6 +86,7 @@ void qio_buffer_reset(QIOBuffer *buffer)
 
 void qio_buffer_free(QIOBuffer *buffer)
 {
+    trace_qio_buffer_free(buffer->name ?: "unnamed", buffer->capacity);
     g_free(buffer->buffer);
     g_free(buffer->name);
     buffer->offset = 0;
@@ -98,6 +110,9 @@ void qio_buffer_advance(QIOBuffer *buffer, size_t len)
 
 void qio_buffer_move_empty(QIOBuffer *to, QIOBuffer *from)
 {
+    trace_qio_buffer_move_empty(to->name ?: "unnamed",
+                                from->offset,
+                                from->name ?: "unnamed");
     assert(to->offset == 0);
 
     g_free(to->buffer);
@@ -116,6 +131,10 @@ void qio_buffer_move(QIOBuffer *to, QIOBuffer *from)
         qio_buffer_move_empty(to, from);
         return;
     }
+
+    trace_qio_buffer_move(to->name ?: "unnamed",
+                          from->offset,
+                          from->name ?: "unnamed");
 
     qio_buffer_reserve(to, from->offset);
     qio_buffer_append(to, from->buffer, from->offset);
