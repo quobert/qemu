8d85107ea2cbbbde2e29f83d78336725ca961f5c
Peter Lieven <pl@kamp.de>
2015-10-05 15:12:58 +0200

    io/buffer: factor out buf_req_size
    Signed-off-by: Peter Lieven <pl@kamp.de>

--- io/buffer.c
+++ io/buffer.c
@@ -24,6 +24,13 @@
 #define QIO_BUFFER_MIN_INIT_SIZE     4096
 #define QIO_BUFFER_MIN_SHRINK_SIZE  65536
 
+static size_t buf_req_size(QIOBuffer *buffer, size_t len)
+{
+    return MAX(QIO_BUFFER_MIN_INIT_SIZE,
+               pow2ceil(buffer->offset + len));
+}
+
+
 void qio_buffer_init(QIOBuffer *buffer, const char *name, ...)
 {
     va_list ap;
@@ -61,8 +68,7 @@ void qio_buffer_reserve(QIOBuffer *buffer, size_t len)
 
     if ((buffer->capacity - buffer->offset) < len) {
         old = buffer->capacity;
-        buffer->capacity = pow2ceil(buffer->offset + len);
-        buffer->capacity = MAX(buffer->capacity, QIO_BUFFER_MIN_INIT_SIZE);
+        buffer->capacity = buf_req_size(buffer, len);
         buffer->buffer = g_realloc(buffer->buffer, buffer->capacity);
         trace_qio_buffer_resize(buffer->name ?: "unnamed",
                                 old, buffer->capacity);
